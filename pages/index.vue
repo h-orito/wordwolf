83690e4a40ae2a4e995e97d714ea7d637c87da84
h-orito <16453967+h-orito@users.noreply.github.com>
2021-06-03 15:52:49 +0900

    匿名ログインで部屋を作成できない不具合を修正

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,12 @@
         <h1 class="title is-5">必要なのはブラウザだけ</h1>
         <div class="columns">
           <div class="column">
-            <p class="content">ワードウルフオンラインはワードウルフが無料・専用アプリ不要で気軽に遊べるサービスです。</p>
-            <nuxt-link class="button is-primary" :to="{ path: 'intro' }">ワードウルフとは</nuxt-link>
+            <p class="content">
+              ワードウルフオンラインはワードウルフが無料・専用アプリ不要で気軽に遊べるサービスです。
+            </p>
+            <nuxt-link class="button is-primary" :to="{ path: 'intro' }"
+              >ワードウルフとは</nuxt-link
+            >
           </div>
         </div>
       </div>
@@ -15,15 +19,26 @@
       <div class="container">
         <div v-if="!isLogin">
           <h1 class="title is-5">はじめる</h1>
-          <nuxt-link class="button is-primary" :to="{ path: 'signin' }">ログイン</nuxt-link> 
+          <nuxt-link class="button is-primary" :to="{ path: 'signin' }"
+            >ログイン</nuxt-link
+          >
         </div>
         <div v-if="isLogin">
           <h1 class="title is-5">ようこそ</h1>
           <p v-if="user.email" class="content">{{ user.email }} さん</p>
-          <nuxt-link v-if="user.email" class="button is-primary" :to="{ path: 'user' }">ユーザ情報編集</nuxt-link>
+          <nuxt-link
+            v-if="user.email"
+            class="button is-primary"
+            :to="{ path: 'user' }"
+            >ユーザ情報編集</nuxt-link
+          >
           <button class="button" @click="logout">ログアウト</button>
-          <div v-if="isLogin && user.email && !user.emailVerified" class="notification is-warning is-size-7" style="margin-top: 1.5rem;">
-            部屋の作成やゲームへの参加にはメール認証が必要です。メールをご確認ください。送信されていない場合は「ユーザ情報編集」から再送してください。<br>
+          <div
+            v-if="isLogin && user.email && !user.emailVerified"
+            class="notification is-warning is-size-7"
+            style="margin-top: 1.5rem;"
+          >
+            部屋の作成やゲームへの参加にはメール認証が必要です。メールをご確認ください。送信されていない場合は「ユーザ情報編集」から再送してください。<br />
           </div>
         </div>
       </div>
@@ -31,8 +46,12 @@
     <section class="section">
       <div class="container">
         <h1 class="title is-5">部屋一覧</h1>
-        <p class="content" v-if="rooms.length == 0">現在部屋がありません。</p><br>
-        <table v-if="rooms.length > 0" class="table is-striped is-fullwidth is-size-7">
+        <p class="content" v-if="rooms.length == 0">現在部屋がありません。</p>
+        <br />
+        <table
+          v-if="rooms.length > 0"
+          class="table is-striped is-fullwidth is-size-7"
+        >
           <thead>
             <tr>
               <th class="has-text-left">部屋名</th>
@@ -41,13 +60,24 @@
             </tr>
           </thead>
           <tbody>
-            <tr
-              v-for="room in rooms" 
-              :key="room['key']">
+            <tr v-for="room in rooms" :key="room['key']">
               <td class="has-text-left">
-                <span v-if="room['roomPassword'] != null && room['roomPassword'] !== ''" class="fas fa-key"></span>
-                <span v-if="room['roomRating'] === 'R15' || room['roomRating'] === 'R18'" class="tag reverse-danger">{{ room['roomRating'] }}</span>
-                <nuxt-link :to="{ path: 'room', query: { id: room.key }}">{{ room.name }}</nuxt-link>
+                <span
+                  v-if="
+                    room['roomPassword'] != null && room['roomPassword'] !== ''
+                  "
+                  class="fas fa-key"
+                ></span>
+                <span
+                  v-if="
+                    room['roomRating'] === 'R15' || room['roomRating'] === 'R18'
+                  "
+                  class="tag reverse-danger"
+                  >{{ room['roomRating'] }}</span
+                >
+                <nuxt-link :to="{ path: 'room', query: { id: room.key } }">{{
+                  room.name
+                }}</nuxt-link>
               </td>
               <td>{{ room['creatorName'] }}</td>
               <td>{{ room['membersNum'] }}人</td>
@@ -56,11 +86,12 @@
         </table>
         <nuxt-link
           class="button is-primary"
-          v-if="isLogin && user.emailVerified" 
-          to="/create-room">部屋を作成</nuxt-link>
+          v-if="canCreateRoom"
+          to="/create-room"
+          >部屋を作成</nuxt-link
+        >
         <div style="margin-top: 15px;">
-          <nuxt-link
-            to="/room-list">終了した部屋を見る</nuxt-link>
+          <nuxt-link to="/room-list">終了した部屋を見る</nuxt-link>
         </div>
       </div>
     </section>
@@ -69,7 +100,9 @@
         <h1 class="title is-5">よくある質問</h1>
         <div class="columns">
           <div class="column">
-            <nuxt-link class="button is-primary" :to="{ path: 'faq' }">よくある質問を見る</nuxt-link>
+            <nuxt-link class="button is-primary" :to="{ path: 'faq' }"
+              >よくある質問を見る</nuxt-link
+            >
           </div>
         </div>
       </div>
@@ -79,10 +112,15 @@
         <h1 class="title is-5">更新情報</h1>
         <div class="columns">
           <div class="column">
-            <ul class="content has-text-left is-size-7" style="list-style: inside;">
+            <ul
+              class="content has-text-left is-size-7"
+              style="list-style: inside;"
+            >
               <li>2021/05/22 匿名ログインできるように変更</li>
             </ul>
-            <nuxt-link class="button is-primary" :to="{ path: 'release-note' }">過去の更新情報を見る</nuxt-link>
+            <nuxt-link class="button is-primary" :to="{ path: 'release-note' }"
+              >過去の更新情報を見る</nuxt-link
+            >
           </div>
         </div>
       </div>
@@ -92,12 +130,41 @@
         <h1 class="title is-5">その他情報</h1>
         <div class="columns">
           <div class="column">
-            <ul class="content has-text-left is-size-7" style="list-style: inside;">
-              <li>ワードウルフは<a href="https://twitter.com/kawasakifactory" target="_blank">川崎晋</a>様が考案したゲームです</li>
-              <li>要望、改善提案、不具合報告はTwitter<a href="https://twitter.com/ort_dev" target="_blank">@ort_dev</a>へお願いします</li>
-              <li>投げ銭いただける方は<a href="javascript:void(0);" @click="openKampaModal">こちら</a>からお願いします</li>
-              <li><a href="javascript:void(0);" @click="openTermsModal">利用規約</a></li>
-              <li><a href="javascript:void(0);" @click="openPolicyModal">プライバシーポリシー</a></li>
+            <ul
+              class="content has-text-left is-size-7"
+              style="list-style: inside;"
+            >
+              <li>
+                ワードウルフは<a
+                  href="https://twitter.com/kawasakifactory"
+                  target="_blank"
+                  >川崎晋</a
+                >様が考案したゲームです
+              </li>
+              <li>
+                要望、改善提案、不具合報告はTwitter<a
+                  href="https://twitter.com/ort_dev"
+                  target="_blank"
+                  >@ort_dev</a
+                >へお願いします
+              </li>
+              <li>
+                投げ銭いただける方は<a
+                  href="javascript:void(0);"
+                  @click="openKampaModal"
+                  >こちら</a
+                >からお願いします
+              </li>
+              <li>
+                <a href="javascript:void(0);" @click="openTermsModal"
+                  >利用規約</a
+                >
+              </li>
+              <li>
+                <a href="javascript:void(0);" @click="openPolicyModal"
+                  >プライバシーポリシー</a
+                >
+              </li>
             </ul>
           </div>
         </div>
@@ -107,36 +174,51 @@
             <div class="box">
               <h4 class="is-size-5">投げ銭について</h4>
               <ul class="is-size-7 content">
-                <li>Amazonギフトカードによる投げ銭（15円〜、手数料なし）になります。</li>
-                <li>Kampa!というサービスを利用することで、個人情報をやりとりすることなくAmazonギフトカードで投げ銭することができます。</li>
-                <li>投げ銭していただける方は下記手順にてお願いします。
-                  <br>
+                <li>
+                  Amazonギフトカードによる投げ銭（15円〜、手数料なし）になります。
+                </li>
+                <li>
+                  Kampa!というサービスを利用することで、個人情報をやりとりすることなくAmazonギフトカードで投げ銭することができます。
+                </li>
+                <li>
+                  投げ銭していただける方は下記手順にてお願いします。
+                  <br />
                   <ol>
-                    <li>最下段の「Amazonギフトカードで投げ銭」よりKampa!サイトへ遷移</li>
-                    <li>表示されたページのメールアドレスをコピーし、Kampa!ボタンをクリック</li>
-                    <li>「金額」に投げ銭していただける金額を入力、「受取人」にコピーしたメールアドレスを貼り付けし、購入 （応援メッセージをいただけると喜びます）</li>
+                    <li>
+                      最下段の「Amazonギフトカードで投げ銭」よりKampa!サイトへ遷移
+                    </li>
+                    <li>
+                      表示されたページのメールアドレスをコピーし、Kampa!ボタンをクリック
+                    </li>
+                    <li>
+                      「金額」に投げ銭していただける金額を入力、「受取人」にコピーしたメールアドレスを貼り付けし、購入
+                      （応援メッセージをいただけると喜びます）
+                    </li>
                   </ol>
                 </li>
-                <li
-                  class="has-text-danger"
-                >Amazonページ遷移時、デフォルトは10,000円に設定されており、 Amazonにログインしている場合は贈り主に氏名が自動入力されているようなのでご注意ください。※金額は15円〜で設定可能です。</li>
+                <li class="has-text-danger">
+                  Amazonページ遷移時、デフォルトは10,000円に設定されており、
+                  Amazonにログインしている場合は贈り主に氏名が自動入力されているようなのでご注意ください。※金額は15円〜で設定可能です。
+                </li>
               </ul>
               <h4 class="is-size-5">補足</h4>
               <ul class="is-size-7 content">
-                <li>頂いた投げ銭はサーバ維持費用など当サイト運営目的にのみ使用します。</li>
+                <li>
+                  頂いた投げ銭はサーバ維持費用など当サイト運営目的にのみ使用します。
+                </li>
                 <li>
                   頂いた改善提案、ご要望については投げ銭の有無に関係なく積極的に取り入れていくので、Twitter
-                  <a
-                    href="https://twitter.com/ort_dev"
-                    target="_blank"
-                  >@ort_dev</a>までお願いします。
+                  <a href="https://twitter.com/ort_dev" target="_blank"
+                    >@ort_dev</a
+                  >までお願いします。
                 </li>
               </ul>
               <a
                 href="http://kampa.me/t/lxc"
                 target="_blank"
                 class="button is-primary"
-              >Amazonギフトカードで投げ銭する</a>
+                >Amazonギフトカードで投げ銭する</a
+              >
             </div>
           </div>
         </div>
@@ -194,6 +276,10 @@ export default {
     },
     isLogin() {
       return this.$store.getters.isLogin
+    },
+    canCreateRoom() {
+      if (!this.isLogin) return false
+      return !this.user.email || this.user.emailVerified
     }
   },
   async fetch({ store }) {
